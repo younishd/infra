08283df18c3a01a7477b37dfbf853e155d53ec00
Younis Bensalah <younis.bensalah@gmail.com>
2023-07-28 20:33:50 +0200

    I'm about to cry

--- terraform/output.tf
+++ terraform/output.tf
@@ -2,11 +2,11 @@ resource "local_file" "ssh_config" {
   content = templatefile("ssh_config.tftpl", {
     "jumphost" : {
       name : hcloud_server.control_plane[0].name,
-      public_ipv4 : hcloud_primary_ip.control_plane_ipv4[0].ip_address
+      address_ipv4 : hcloud_primary_ip.control_plane_ipv4[0].ip_address
     },
     "hosts" : [for v in concat(slice(hcloud_server.control_plane, 1, length(hcloud_server.control_plane)), hcloud_server.data_plane) : {
       name : v.name,
-      private_ipv4 : tolist(v.network)[0].ip
+      address_ipv4 : tolist(v.network)[0].ip
     }]
   })
   filename = "../k8s/inventory/hcloud/ssh_config"
@@ -16,13 +16,11 @@ resource "local_file" "inventory" {
   content = templatefile("inventory.tftpl", {
     "control_plane" : [for v in hcloud_server.control_plane : {
       name : v.name,
-      private_ipv4 : tolist(v.network)[0].ip,
-      public_ipv4 : v.ipv4_address
+      address_ipv4 : tolist(v.network)[0].ip
     }],
     "data_plane" : [for v in hcloud_server.data_plane : {
       name : v.name,
-      private_ipv4 : tolist(v.network)[0].ip,
-      public_ipv4 : v.ipv4_address
+      address_ipv4 : tolist(v.network)[0].ip
     }]
   })
   filename = "../k8s/inventory/hcloud/main.yaml"
