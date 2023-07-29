ea9355e86aa399ee24cd6843725f182b71d664bc
Younis Bensalah <younis.bensalah@gmail.com>
2023-07-29 17:10:08 +0200

    Move generated ssh config into ssh/

--- cluster/terraform/output.tf
+++ cluster/terraform/output.tf
@@ -4,26 +4,32 @@ resource "local_file" "ssh_config" {
       name : hcloud_server.control_plane[0].name,
       public_ipv4 : hcloud_primary_ip.control_plane_ipv4[0].ip_address
     },
-    "hosts" : [for v in concat(slice(hcloud_server.control_plane, 1, length(hcloud_server.control_plane)), hcloud_server.data_plane) : {
-      name : v.name,
-      private_ipv4 : tolist(v.network)[0].ip
-    }]
+    "hosts" : [
+      for v in concat(slice(hcloud_server.control_plane, 1, length(hcloud_server.control_plane)), hcloud_server.data_plane) : {
+        name : v.name,
+        private_ipv4 : tolist(v.network)[0].ip
+      }
+    ]
   })
-  filename = "../k8s/inventory/hcloud/ssh_config"
+  filename = "../ssh/hcloud.config"
 }
 
 resource "local_file" "inventory" {
   content = templatefile("inventory.tftpl", {
-    "control_plane" : [for v in hcloud_server.control_plane : {
-      name : v.name,
-      private_ipv4 : tolist(v.network)[0].ip
-    }],
-    "data_plane" : [for v in hcloud_server.data_plane : {
-      name : v.name,
-      private_ipv4 : tolist(v.network)[0].ip
-    }]
+    "control_plane" : [
+      for v in hcloud_server.control_plane : {
+        name : v.name,
+        private_ipv4 : tolist(v.network)[0].ip
+      }
+    ],
+    "data_plane" : [
+      for v in hcloud_server.data_plane : {
+        name : v.name,
+        private_ipv4 : tolist(v.network)[0].ip
+      }
+    ]
   })
-  filename = "../k8s/inventory/hcloud/main.yaml"
+  filename = "../inventory/hcloud/main.yaml"
 }
 
 output "public_ipv4" {
