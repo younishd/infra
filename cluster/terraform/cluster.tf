843ccafbae2261c72dc7851c6e02fb6c126fb17f
Younis Bensalah <younis.bensalah@gmail.com>
2023-07-30 20:39:49 +0200

    Literally whatever

--- cluster/terraform/cluster.tf
+++ cluster/terraform/cluster.tf
@@ -1,5 +1,5 @@
 resource "hcloud_server" "control_plane" {
-  count = local.count_control_plane
+  count = var.count_control_plane
 
   name        = "master-${count.index}"
   datacenter  = "fsn1-dc14"
@@ -25,7 +25,7 @@ resource "hcloud_server" "control_plane" {
 }
 
 resource "hcloud_server" "data_plane" {
-  count = local.count_data_plane
+  count = var.count_data_plane
 
   name        = "worker-${count.index}"
   datacenter  = "fsn1-dc14"
@@ -41,7 +41,7 @@ resource "hcloud_server" "data_plane" {
 
   network {
     network_id = hcloud_network.private.id
-    ip         = cidrhost("10.0.0.0/16", count.index + local.count_control_plane + 2)
+    ip         = cidrhost("10.0.0.0/16", count.index + var.count_control_plane + 2)
   }
 
   depends_on = [
@@ -51,7 +51,7 @@ resource "hcloud_server" "data_plane" {
 }
 
 resource "hcloud_primary_ip" "control_plane_ipv4" {
-  count = local.count_control_plane
+  count = var.count_control_plane
 
   name          = "master-${count.index}-ipv4"
   datacenter    = "fsn1-dc14"
@@ -61,7 +61,7 @@ resource "hcloud_primary_ip" "control_plane_ipv4" {
 }
 
 resource "hcloud_primary_ip" "data_plane_ipv4" {
-  count = local.count_data_plane
+  count = var.count_data_plane
 
   name          = "worker-${count.index}-ipv4"
   datacenter    = "fsn1-dc14"
@@ -80,9 +80,4 @@ resource "hcloud_network_subnet" "private" {
   network_id   = hcloud_network.private.id
   network_zone = "eu-central"
   ip_range     = "10.0.0.0/16"
-}
-
-locals {
-  count_control_plane = 1
-  count_data_plane    = 1
 }
--- cluster/terraform/variables.tf
+++ cluster/terraform/variables.tf
@@ -1,13 +1,25 @@
 variable "control_plane_server_type" {
   description = "Control plane server type"
   type        = string
-  default     = "cax21"
+  default     = "cax41"
 }
 
 variable "date_plane_server_type" {
   description = "Data plane server type"
   type        = string
   default     = "cax31"
+}
+
+variable "count_control_plane" {
+  description = "Number of control plane servers"
+  type        = number
+  default     = 1
+}
+
+variable "count_data_plane" {
+  description = "Number of data plane servers"
+  type        = number
+  default     = 0
 }
 
 variable "hcloud_token" {
