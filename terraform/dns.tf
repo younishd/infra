f0b7d4ba5896a6313f40dc0d42410da90721480e
Younis Bensalah <younis.bensalah@gmail.com>
2023-12-27 22:49:53 +0100

    Add wildcard DNS record

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -11,3 +11,13 @@ resource "gandi_livedns_record" "zenshift_io" {
     hcloud_primary_ip.public[0].ip_address
   ]
 }
+
+resource "gandi_livedns_record" "zenshift_io_subdomains" {
+  zone = data.gandi_domain.zenshift_io.id
+  name = "*"
+  type = "A"
+  ttl  = 3600
+  values = [
+    hcloud_primary_ip.public[0].ip_address
+  ]
+}
