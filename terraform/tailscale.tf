af9cccb58dcf85a4f59006e40f47e3c5e5a61c3a
Younis Bensalah <younis.bensalah@gmail.com>
2024-02-12 14:21:18 +0100

    Delete terraform

--- terraform/tailscale.tf
+++
@@ -1,8 +0,0 @@
-resource "tailscale_tailnet_key" "cluster" {
-  reusable      = true
-  ephemeral     = false
-  preauthorized = true
-  expiry        = 3600
-  description   = "Cluster key"
-  tags          = ["tag:cluster"]
-}
