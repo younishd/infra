04939132d558dbf9cf339bf96efbc25cfab41c4a
Younis Bensalah <younis.bensalah@gmail.com>
2025-02-12 18:38:20 +0000

    Revert "Delete terraform"
    This reverts commit af9cccb58dcf85a4f59006e40f47e3c5e5a61c3a.

---
+++ terraform/dns.tf
@@ -0,0 +1,19 @@
+data "gandi_domain" "zenshift_io" {
+  name = "zenshift.io"
+}
+
+resource "gandi_livedns_record" "zenshift_io" {
+  zone   = data.gandi_domain.zenshift_io.id
+  name   = "@"
+  type   = "A"
+  ttl    = 3600
+  values = hcloud_primary_ip.public[*].ip_address
+}
+
+resource "gandi_livedns_record" "zenshift_io_subdomains" {
+  zone   = data.gandi_domain.zenshift_io.id
+  name   = "*"
+  type   = "A"
+  ttl    = 3600
+  values = hcloud_primary_ip.public[*].ip_address
+}
